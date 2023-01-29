5ec40845d0492f5ebd6a79b49b43c0d0b8cc8d3e
wynn719 <762671261@qq.com>
2023-01-29 19:20:28 +0800

    feat: init

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "@/components/layout";
+import { marked } from "marked";
+import { readFile } from "fs/promises";
+import path from 'path'
 
-export default function FirstPost() {
+interface PostProps {
+  postContent: string;
+}
+
+export default function FirstPost({ postContent }: PostProps) {
+  const markdown = marked(postContent);
+
   return (
     <Layout home={false}>
       <Head>
@@ -10,6 +19,19 @@ export default function FirstPost() {
       <h2>
         Post content
       </h2>
+      <div dangerouslySetInnerHTML={{ __html: markdown }}></div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.resolve('./src/posts/2015-02-13-my-blog-version1.0-description.md')
+  const postFile = await readFile(filePath);
+  const postContent = postFile.toString();
+
+  return {
+    props: {
+      postContent,
+    }
+  }
 }
