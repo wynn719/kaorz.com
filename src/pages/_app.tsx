900dda9d4fb2b3a3e86e974f86dbd7831274a539
wynn719 <762671261@qq.com>
2023-06-12 19:15:09 +0800

    feat: add progress-bar for page change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import NextProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextProgress
+        color="#63a67f"
+        height={1}
+        options={{ showSpinner: false }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
